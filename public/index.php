d6e6e701f29cbec9fb4ef39fe79a889a8901b674
Al-Saleh KEITA <alsaleh.keita@gmail.com>
2025-11-08 00:00:55 +0100

    Take into account environment in server superglobal

--- public/index.php
+++ public/index.php
@@ -5,8 +5,15 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
-// Charger les variables d'environnement depuis les fichiers .env
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// Load environment variables from .env file, if it exists; otherwise, rely on the server environment
+$envFile = dirname(__DIR__).'/.env';
+if (is_readable($envFile)) {
+    (new Dotenv())->loadEnv($envFile);
+} else {
+    // Fallback to loading from server environment
+    $_SERVER['APP_ENV'] = $_SERVER['APP_ENV'] ?? 'dev';
+    $_SERVER['APP_DEBUG'] = $_SERVER['APP_DEBUG'] ?? '1';
+}
 
 $kernel = new Kernel($_SERVER['APP_ENV'] ?? 'dev', (bool) ($_SERVER['APP_DEBUG'] ?? true));
 $request = Symfony\Component\HttpFoundation\Request::createFromGlobals();
